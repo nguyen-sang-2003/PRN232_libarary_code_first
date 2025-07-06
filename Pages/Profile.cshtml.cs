22cd6d1af84bc27820887649cf593de71cfe0202
shioko <ichisadashioko@gmail.com>
2025-07-06 10:48:40 -0400

    wip using token razor

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -18,25 +19,60 @@ namespace LibararyWebApplication.Pages
 
                 string current_host = HttpContext.Request.Host.ToString();
 
-                 existing_token = Request.Headers.Authorization;
+                existing_token = Request.Headers.Authorization;
+                if (existing_token == null)// remove bearer
+                {
+                    // get token from cookie
+                    existing_token = Request.Cookies["token"];
+                }
                 if (existing_token == null)
                 {
                     Redirect($"http://{current_host}/login");
                 }
 
-                int testUserId = 1;
+                // set header for httpClient
+                httpClient.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", existing_token);
+                var json_user_info = await httpClient.GetStringAsync(
+                    $"{api_endpoint}/api/users/info");
+                var user_info = JsonConvert.DeserializeObject<dynamic>(json_user_info);
+                System.Console.WriteLine($"User info: {json_user_info}");
 
-                var response = await httpClient.GetAsync($"{api_endpoint}/api/Users/{testUserId}");
-
-                if (response.IsSuccessStatusCode)
+                if (user_info == null || user_info.Name == null)
                 {
-                    var json = await response.Content.ReadAsStringAsync();
-                    User = JsonConvert.DeserializeObject<User>(json);
+                    // Redirect to login if user info is not available
+                    var returnUrl = HttpContext.Request.Path + HttpContext.Request.QueryString;
+                    Redirect($"/login?return_url={Uri.EscapeDataString(returnUrl)}");
+                }
+
+                string user_name = user_info.Name.ToString();
+                // Check if the user exists in the database
+                var ctx = new PrnContext();
+                var user_obj = ctx.Users.FirstOrDefault(u => u.Username == user_name);
+                if (user_obj == null)
+                {
+                    // User not found, redirect to login
+                    var returnUrl = HttpContext.Request.Path + HttpContext.Request.QueryString;
+                    Redirect($"/login?return_url={Uri.EscapeDataString(returnUrl)}");
                 }
                 else
                 {
-                    Console.WriteLine($"Lỗi khi gọi API: {response.StatusCode}");
+                    User = user_obj;
                 }
+
+                // int testUserId = 1;
+
+                // var response = await httpClient.GetAsync($"{api_endpoint}/api/Users/{testUserId}");
+
+                // if (response.IsSuccessStatusCode)
+                // {
+                //     var json = await response.Content.ReadAsStringAsync();
+                //     User = JsonConvert.DeserializeObject<User>(json);
+                // }
+                // else
+                // {
+                //     Console.WriteLine($"Lỗi khi gọi API: {response.StatusCode}");
+                // }
             }
             catch (Exception ex)
             {
