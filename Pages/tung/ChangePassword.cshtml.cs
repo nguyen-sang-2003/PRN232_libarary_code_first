400bd0ca1abfb368065e5d74fe9f8ba67f34e11f
shioko <ichisadashioko@gmail.com>
2025-07-21 18:27:33 +0700

    WIP: users: change password

---
+++ Pages/tung/ChangePassword.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using tung;
+
+namespace LibararyWebApplication.Pages
+{
+    public class ChangePasswordModel : PageModel
+    {
+        public readonly HttpClient client;
+        public ChangePasswordModel(HttpClient client)
+        {
+            this.client = client;
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            var existing_token = Utils.get_access_token(Request);
+            if (string.IsNullOrWhiteSpace(existing_token))
+            {
+                return Redirect($"/login?return_url={System.Web.HttpUtility.UrlEncode(HttpContext.Request.Path)}");
+            }
+
+            var user = Utils.get_user_from_token(existing_token, client);
+            string request_url = $"{Request.Scheme}://{Request.Host}/api/users/fromtoken";
+            return Page();
+        }
+    }
+}
---
+++ Pages/tung/Utils.cs
@@ -0,0 +1,30 @@
+namespace tung
+{
+    public class Utils
+    {
+        public static string? get_access_token(HttpRequest Request)
+        {
+            string? existing_token = Request.Headers.Authorization;
+            if (string.IsNullOrEmpty(existing_token))
+            {
+                existing_token = Request.Cookies["token"];
+            }
+
+            if (string.IsNullOrEmpty(existing_token))
+            {
+                return null;
+            }
+
+            if (existing_token.StartsWith("Bearer "))
+            {
+                existing_token = existing_token.Substring("Bearer ".Length);
+            }
+            return existing_token;
+        }
+
+        //public static User? get_user_from_token(string token, HttpClient client, string request_url)
+        //{
+        //    return null;
+        //}
+    }
+}
